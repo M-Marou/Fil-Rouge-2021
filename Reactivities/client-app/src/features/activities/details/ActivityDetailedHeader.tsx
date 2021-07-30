4d5c07b1b5e1c4f56b14058a7bb3c12252f0e759
Marouane Moumni <57830533+M-Marou@users.noreply.github.com>
2021-07-30 12:22:27 +0100

    Added Form Validation

--- Reactivities/client-app/src/features/activities/details/ActivityDetailedHeader.tsx
+++ Reactivities/client-app/src/features/activities/details/ActivityDetailedHeader.tsx
@@ -1,7 +1,9 @@
 import { observer } from 'mobx-react-lite';
 import React from 'react'
+import { Link } from 'react-router-dom';
 import {Button, Header, Item, Segment, Image} from 'semantic-ui-react'
 import {Activity} from "../../../app/models/activity";
+import {format} from 'date-fns';
 
 const activityImageStyle = {
     filter: 'brightness(30%)'
@@ -34,7 +36,7 @@ export default observer (function ActivityDetailedHeader({activity}: Props) {
                                     content={activity.title}
                                     style={{color: 'white'}}
                                 />
-                                <p>{activity.date}</p>
+                                <p>{format (activity.date!, 'dd MMM yyyy')}</p>
                                 <p>
                                     Hosted by <strong>Bob</strong>
                                 </p>
@@ -46,7 +48,7 @@ export default observer (function ActivityDetailedHeader({activity}: Props) {
             <Segment clearing attached='bottom'>
                 <Button color='violet'>Join Activity</Button>
                 <Button>Cancel attendance</Button>
-                <Button inverted color='violet' floated='right'>
+                <Button as={Link} to={`/manage/${activity.id}`} inverted color='violet' floated='right'>
                     Manage Event
                 </Button>
             </Segment>
